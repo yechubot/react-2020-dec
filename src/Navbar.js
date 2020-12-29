e339e60195d91ba5d4697b5fe248b305cb0e9028
yechu <yechukim217@gmail.com>
2020-12-30 00:29:20 +0900

    style added

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,17 @@ const Navbar = () => {//sfc -- stateless function
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
+            <div className="links">
             <a href="/">Home</a>
-            <a href="/create">New Blog</a>
+            <a href="/create" style ={//dynamic value는 {}로 감싸고,
+               {// 이 안의 또 다른 {}는 object를 위한 것 
+                color: "white",
+                backgroundColor: "#f1356d",
+                borderRadius: '8px'
+                //inline - styling 은 이렇게 하면 된다. 
+               }
+            }>New Blog</a>
+            </div>
         </nav>
      );
 }
