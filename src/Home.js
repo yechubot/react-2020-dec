132587d71e29e230ee40e1055d51f63609a6a6d7
yechu <yechukim217@gmail.com>
2021-01-01 01:59:19 +0900

    #10

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,23 @@
 import { useState } from 'react';
-//useState hook을 사용하여 리액트가  state 변동하면 렌더링 할 수 있게..?
-//reactive value가 필요하면 이를 사용함 
-const Home = () => {
-    //let name = 'mario';
-    const [name, setName] = useState('mario');
-    const[age, setAge] = useState(25); //initial value
-    //value, setFunction 
-    const handleClick = () => {
-        setName('luigi');
-        setAge('30');
-    }
 
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+    {title:'my mew website', body: 'lorem ipsum...', author:'mario', id:1},
+    {title:'welcome party', body: 'lorem ipsum...', author:'yechu', id:2},
+    {title:'my web dev top tips', body: 'lorem ipsum...', author:'ninja', id:3}
+    ]);
+    
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old </p>
-            
-            <button onClick={handleClick}>Click me</button>
-
+            {blogs.map((blog)=> (//map으로 할때 해당 아이템은 key property꼭 있어야 한다
+            //그리고 키는 유니크해야함 
+                <div className="blog-preview" key ={blog.id}>
+                    <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
-}
+    }
+
 export default Home;
